8cc011d5ad6868753adc00241c9b408097b224b9
vastlaan <vastlaan@mail.com>
2020-10-19 18:49:45 +0200

    Add conditional return to server config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,24 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT"),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET"),
-    },
-  },
-});
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      host: env("HOST", "0.0.0.0"),
+      port: env.int("PORT"),
+      admin: {
+        auth: {
+          secret: env("ADMIN_JWT_SECRET"),
+        },
+      },
+      url: "https://api.itcontext.nl",
+    };
+  } else {
+    return {
+      host: env("HOST", "0.0.0.0"),
+      port: env.int("PORT"),
+      admin: {
+        auth: {
+          secret: env("ADMIN_JWT_SECRET"),
+        },
+      },
+    };
+  }
+};
